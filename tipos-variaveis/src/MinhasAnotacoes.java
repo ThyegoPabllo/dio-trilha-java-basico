10251c05b3b8ffe0c74b43687b4676802dab692c
Thyego <Thyego.pabllo2004@gmail.com>
2024-08-22 16:38:05 -0300

    Brincando com operadores

---
+++ tipos-variaveis/src/MinhasAnotacoes.java
@@ -0,0 +1,20 @@
+public class MinhasAnotacoes {
+    
+//Estudar os tipos primitivos
+//Estudar a classe String que representa texto na aplicação
+
+public static void main(String[] args) {
+    
+    String meuNome = "Thyego Pabllo";
+
+    System.out.println(meuNome);
+    
+
+
+}
+
+
+
+
+
+}
