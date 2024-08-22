f34abdc7c76011f973f9954ee779fe66b80a8e04
Thyego <Thyego.pabllo2004@gmail.com>
2024-08-22 17:41:32 -0300

    tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,5 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        double salarioMinimo = 2500;
 
         short numeroCurto = 1;
         int  numeroNormal = numeroCurto;
